2e52850a3e219939801492b8e6971aabfd306c7d
Anna Liu <aliu426@uwo.ca>
2024-12-01 16:11:10 -0500

    connection to db

---
+++ SRC/server/server.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "recipe_app"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+//
